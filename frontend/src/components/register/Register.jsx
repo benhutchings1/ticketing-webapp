f9d7f212107cf3d07176bbb7cd8cdd7ef7d4d010
foxr1 <45184856+foxr1@users.noreply.github.com>
2023-03-07 16:39:00 +0000

    First commit for Flask/React - Template for routes in app.py for login/register/logout - Simple pages for login/register/home setup with React routing - Requests to Flask side from React setup (need work)

---
+++ frontend/src/components/register/Register.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import httpClient from "../../httpClient";
+import {useNavigate} from "react-router-dom";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const registerUser = async () => {
+    try {
+      const resp = await httpClient.post("//localhost:5000/register", {
+        email,
+        password,
+      });
+
+      navigate("/")
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert("Invalid credentials");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create an account</h1>
+      <form>
+        <div>
+          <label>Email: </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id=""
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id=""
+          />
+        </div>
+        <button type="button" onClick={() => registerUser()}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
