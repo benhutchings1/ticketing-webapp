39e5c98cc423556590ed9a031e540db967c65474
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-09 21:24:47 +0000

    Add JWT_CSRF_IN_COOKIES setting in config.py

--- flask-server/config.py
+++ flask-server/config.py
@@ -8,11 +8,13 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 class Config:
     SECRET_KEY = config('SECRET_KEY')
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+    JWT_CSRF_IN_COOKIES = True
     SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
 
 
 class DevConfig(Config):
     SECRET_KEY = config('SECRET_KEY')
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+    JWT_CSRF_IN_COOKIES = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
     DEBUG = True
