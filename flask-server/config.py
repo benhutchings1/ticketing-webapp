0e270f9526258ef662e3e18fadae021c1b103989
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-09 21:50:06 +0000

    Set JWT_TOKEN_LOCATION to "cookies" in config.py

--- flask-server/config.py
+++ flask-server/config.py
@@ -9,6 +9,7 @@ class Config:
     SECRET_KEY = config('SECRET_KEY')
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
     JWT_CSRF_IN_COOKIES = True
+    JWT_TOKEN_LOCATION = "cookies"
     SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
 
 
@@ -16,5 +17,6 @@ class DevConfig(Config):
     SECRET_KEY = config('SECRET_KEY')
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
     JWT_CSRF_IN_COOKIES = True
+    JWT_TOKEN_LOCATION = "cookies"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
     DEBUG = True
