31a26dd79ef0b8bf0897251f5762810bc12e98ad
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-09 21:11:04 +0000

    Remove unused imports in app.py

--- flask-server/app.py
+++ flask-server/app.py
@@ -5,8 +5,8 @@ from models import User
 from exts import db
 # from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, set_access_cookies, \
-    unset_jwt_cookies, set_refresh_cookies, get_jwt, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, set_access_cookies, \
+    unset_jwt_cookies, get_jwt, get_jwt_identity
 from datetime import datetime, timedelta, timezone
 
 app = Flask(__name__)
