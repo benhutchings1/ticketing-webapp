2a325262aef04a2d45a5bce84fad0ae85b9dc895
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-09 09:46:27 +0000

    Fix config.py file format

--- flask-server/config.py
+++ flask-server/config.py
@@ -4,11 +4,13 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+
 class Config:
-    SECRET_KEY=config('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(BASE_DIR, 'dev.db')
-    DEBUG=True
-    SQLALCHEMY_ECHO=True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
