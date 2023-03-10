7264cfdabc0217f3f503a2db1bcfd2cb8679218a
Ben <b9008088@newcastle.ac.uk>
2023-03-10 19:39:13 +0000

    Configed tables to match design

--- flask-server/models.py
+++ flask-server/models.py
@@ -3,15 +3,15 @@ from exts import db
 
 # User model
 class User(db.Model):
-    user_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    email_address = db.Column(db.String(100), nullable=False, unique=True)
-    passwd_hash = db.Column(db.String(16), nullable=False)
-    firstname = db.Column(db.String(20), nullable=False)
-    surname = db.Column(db.String(20), nullable=False)
-    date_of_birth = db.Column(db.Date)
-    postcode = db.Column(db.String(7))
-    phone_number = db.Column(db.String(14), unique=True)
-    role = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(        db.Integer(8), primary_key=True, autoincrement=True)
+    firstname = db.Column(      db.String(32), nullable=False)
+    surname = db.Column(        db.String(32), nullable=False)
+    date_of_birth = db.Column(  db.Date)
+    postcode = db.Column(       db.String(8))
+    phone_number = db.Column(   db.String(16), unique=True)
+    email_address = db.Column(  db.String(64), nullable=False, unique=True)
+    role = db.Column(           db.String(16), nullable=False)
+    passwd_hash = db.Column(    db.String(256), nullable=False) 
 
     def save(self):
         db.session.add(self)
@@ -26,44 +26,17 @@ class User(db.Model):
         self.passwd_hash = passwd_hash
         self.postcode = postcode
         self.phone_number = phone_number
-        db.session.commit()
-
-
-# Venue model
-class Venue(db.Model):
-    venue_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(200), nullable=False)
-    postcode = db.Column(db.String(7), nullable=False)
-    capacity = db.Column(db.Integer())
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def update(self, name, location, postcode, capacity):
-        self.name = name
-        self.location = location
-        self.postcode = postcode
-        self.capacity = capacity
-        db.session.commit()
-
-
-
-# Event model
+        db.session.commit()   
+    
+# Event and ticket models
 class Event(db.Model):
-    event_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    venue_id = db.Column(db.Integer(), db.ForeignKey('venue.venue_id'), nullable=False)
-    event_name = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    time = db.Column(db.String(20)) #db.Column(db.Time, nullable=False)   string is easier to marshal/jsonify
-    genre = db.Column(db.String(100))
-    description = db.Column(db.String(1000))  # db.Text() . Overflow?
-    venue = db.relationship(Venue)
+    event_id = db.Column(   db.Integer(8),  primary_key=True, autoincrement=True)
+    event_name = db.Column( db.String(128), nullable=False)
+    venue_id = db.Column(   db.Integer(8),  db.ForeignKey('venue.venue_id'), nullable=False)
+    datetime = db.Column(   db.DateTime,    nullable=False) # I have set this to datetime but can be changed if needed
+    description = db.Column(db.Text)  # db.Text() . Overflow?
+    genre = db.Column(      db.String(128))
+    venueRelate = db.relationship("Venue")
 
     def save(self):
         db.session.add(self)
@@ -81,15 +54,41 @@ class Event(db.Model):
         self.description = description
         db.session.commit()
 
+# Venue model
+class Venue(db.Model):
+    venue_id = db.Column(   db.Integer(8), primary_key=True, autoincrement=True)
+    location = db.Column(   db.String(256), nullable=False)
+    name = db.Column(       db.String(128), nullable=False)
+    postcode = db.Column(   db.String(8), nullable=False)
+    capacity = db.Column(   db.Integer(8))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, name, location, postcode, capacity):
+        self.name = name
+        self.location = location
+        self.postcode = postcode
+        self.capacity = capacity
+        db.session.commit()
+
 
 # model for tickets to be generated for specific user
 class UserTicket(db.Model):
-    ticket_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    event_id = db.Column(db.Integer(), db.ForeignKey('event.event_id'), nullable=False)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'), nullable=False)
-    cipher_key = db.Column(db.String(), nullable=False)
-    ticket_type = db.Column(db.String(), nullable=False)
-    valid = db.Column(db.Boolean, nullable=False)
+    ticket_id = db.Column(      db.Integer(8), primary_key=True, autoincrement=True)
+    event_id = db.Column(       db.Integer(8), db.ForeignKey('event.event_id'), nullable=False)
+    user_id = db.Column(        db.Integer(8), db.ForeignKey('user.user_id'), nullable=False)
+    cipher_key = db.Column(     db.String(256), nullable=False)
+    ticket_type = db.Column(    db.String(64), nullable=False)
+    valid = db.Column(          db.Boolean, nullable=False)
+
+    eventRelate = db.relationship("Event")
+    userRelate = db.relationship("User")
 
     def save(self):
         db.session.add(self)
@@ -101,18 +100,10 @@ class UserTicket(db.Model):
 
 
 class TokenBlocklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(36), nullable=False, index=True)
-    created_at = db.Column(db.DateTime, nullable=False)
-
-class UserTickets(db.Model):
-    ticketID  = db.Column(db.Integer, primary_key=True)
-    eventID = db.Column(db.Integer)
-    ticketTypeID = db.Column(db.Integer)
-    userID = db.Column(db.Integer)
-    cipherkey = db.Column(db.String)
-    valid = db.Column(db.SmallInteger)
+    id = db.Column(         db.Integer(8), primary_key=True)
+    jti = db.Column(        db.String(64), nullable=False, index=True)
+    created_at = db.Column( db.DateTime, nullable=False)
 
 class IdempotencyTokens(db.Model):
-    token = db.Column(db.String, primary_key=True)
-    valid = db.Column(db.SmallInteger)
+    token = db.Column(      db.String(128), primary_key=True, index=True)
+    valid = db.Column(      db.Integer(1))
