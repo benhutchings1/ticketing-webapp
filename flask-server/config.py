3200555619b082eed7361df81bf40b3970622c51
Ben <b9008088@newcastle.ac.uk>
2023-03-10 19:42:05 +0000

    Updated connection URI

--- flask-server/config.py
+++ flask-server/config.py
@@ -10,7 +10,7 @@ class Config:
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
     JWT_CSRF_IN_COOKIES = True
     JWT_TOKEN_LOCATION = "cookies"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'main.db')
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://group4:TruckForumUpset1!@92.239.221.181 :3306/ticketing_app" #"sqlite:///" + os.path.join(BASE_DIR, 'main.db')
 
 
 class DevConfig(Config):
@@ -18,7 +18,7 @@ class DevConfig(Config):
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
     JWT_CSRF_IN_COOKIES = True
     JWT_TOKEN_LOCATION = "cookies"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://group4:TruckForumUpset1!@92.239.221.181 :3306/ticketing_app" #"sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
     DEBUG = True
 
 
