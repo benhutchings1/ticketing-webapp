4854480ec9124e5a5484068f53ea6785e9fcc433
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2023-03-19 18:58:25 +0000

    Add signup unit tests

--- flask-server/test/test_flask-server.py
+++ flask-server/test/test_flask-server.py
@@ -48,6 +48,142 @@ class Tests(unittest.TestCase):
 
         self.assertEqual(200, response.status_code)
 
+    def test_signup_invalid_email(self):
+        data = {
+            "email_address": "noemail",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "34544345"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_password(self):
+        data = {
+            "email_address": "random1@random.com",
+            "password": "test",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "343445345"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_firstname(self):
+        data = {
+            "email_address": "random2@random.com",
+            "password": "test1234",
+            "firstname": "t"*100,
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "33445345"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_dob(self):
+        data = {
+            "email_address": "random3@random.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023",
+            "postcode": "test1234",
+            "phone_number": "345345223"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_postcode(self):
+        data = {
+            "email_address": "random4@random.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "t"*100,
+            "phone_number": "3445"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_phone_number(self):
+        data = {
+            "email_address": "random5@random.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "no"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_invalid_phone_number_2(self):
+        data = {
+            "email_address": "random5@random.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "1"*50
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_duplicate_email(self):
+        data = {
+            "email_address": "test@test.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "123456641"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+
+    def test_signup_duplicate_phone_number(self):
+        data = {
+            "email_address": "random6@test.com",
+            "password": "test1234",
+            "firstname": "test",
+            "surname": "test",
+            "date_of_birth": "2023-03-16",
+            "postcode": "test1234",
+            "phone_number": "07123456789"
+        }
+
+        response = self.app.post('/user/signup', data=json.dumps(data), content_type='application/json')
+
+        self.assertEqual(400, response.status_code)
+        self.assertEqual(400, response.json.get("msg"))
+
     def test_login(self):
         data = {
             "email_address": "test@test.com",
